88f35ce1c94f9d60ce15bee453a7d9bbe81ae6c1
Julyo <julyo110701@gmail.com>
2021-01-20 18:30:31 +0700

    update live search and load indicator

--- script.js
+++ script.js
@@ -105,3 +105,11 @@ function showCovidProvinceData(c) {
             </div>
           </div> `;
 }
+
+// Loading Indicator
+const loading = document.querySelector('.loading');
+window.addEventListener('load', function () {
+  loading.parentElement.removeChild(loading);
+});
+
+
